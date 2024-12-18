d789cf755b68f6ebface24a4170b3661c42990cb
indra saputra idrus <indrasaputraidrus@gmail.com>
2024-12-18 11:45:34 +0000

    chore: init project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../app.css';
+	import type { Snippet } from 'svelte';
+
+	type Props = {
+		children: Snippet<[]>;
+	};
+
+	let props: Props = $props();
+</script>
+
+{@render props.children?.()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,50 @@
+<div class="container">
+	<button class="success">Success</button>
+	<button class="info">Info</button>
+	<button class="warning">Warning</button>
+	<button class="error">Error</button>
+</div>
+
+<style>
+	.container {
+		width: 18rem;
+		display: grid;
+		gap: 1rem;
+		grid-template-rows: repeat(4, 1fr);
+		padding: 1rem;
+		background-color: var(--secondary);
+		border-radius: 0.5rem;
+
+		button {
+			padding: 10px 0px;
+			border: none;
+			color: var(--secondary);
+			font-weight: 600;
+			border-radius: 0.375rem;
+
+			&:hover {
+				opacity: 85%;
+			}
+
+			&:focus {
+				outline: none;
+			}
+
+			&.success {
+				background-color: var(--toast-success);
+			}
+
+			&.info {
+				background-color: var(--toast-info);
+			}
+
+			&.warning {
+				background-color: var(--toast-warning);
+			}
+
+			&.error {
+				background-color: var(--toast-error);
+			}
+		}
+	}
+</style>
